c8c831d26860257bc8b2c7d8101e5d5bfa82788a
Avinash Agrawal <avinash8526@gmail.com>
2013-07-20 02:23:39 +0530

    Version 1.0
    Ready for Production

---
+++ list2csv.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin
+
+class list2csvCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		sels = self.view.sel()		
+		if self.isRegionEmpty(sels[0]):
+			sublime.message_dialog("Please select list by pressing ctrl+A")
+		else:
+			if self.convertListTocsv(sels,edit) =="true":
+				sublime.status_message("Conversion Done")
+
+			
+	def isRegionEmpty(self,region):
+		return region.empty()
+
+	def convertListTocsv(self,selection,edt):
+		for se in selection:			
+			positions =  self.view.lines(se)
+			for pos in positions:
+				if self.view.find('\n',pos.end()):
+					nwlnreg = self.view.find('\n',pos.end())
+					self.view.replace(edt, nwlnreg, ',\r')
+		return "true"			 
+		
