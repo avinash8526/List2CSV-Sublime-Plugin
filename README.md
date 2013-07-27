4b476538feb1e4e483e211bb0cd7c42bbdababd9
Avinash Agrawal <avinash8526@gmail.com>
2013-07-28 01:50:57 +0530

    Version 2.0
    Added Pattern defining feature, Setting Menu.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-List2CSV-Sublime-Plugin
+List2CSV-Sublime-Plugin 2.0
 =======================
 
 Plugin for creating comma separated value from a list of data
@@ -38,7 +38,20 @@ For manual install
 How to use
 ----------
 
+
+- Basic Use
+
 If you have a list , select all and press `ctrl`+`alt`+`,` or right click on mouse and select option "Convert List To CSV"
-watch a `youtube` demo on how to use it `http://www.youtube.com/watch?v=W1HLoRRxKRk`
+watch a `youtube` demo on how to use it `http://www.youtube.com/watch?v=W1HLoRRxKRk
+
+- Advance Use (New Features added for 2.0)
+
+Change the pattern by hitting `ctrl`+`alt`+`.` , you will see input panel `{'SecondDel':'','ShowPop':'No','Separator':',','FirstDel':'','Memory':'No'}` now if you want some pattern like `"123","1234"` instead of `123,1234` then change the string as {'SecondDel':'`"`','ShowPop':'No','Separator':',','FirstDel':'`"`','Memory':'No'}. If you want to maintain the pattern for whole session change the `Memory` value from `No` to `Yes`.
+Watch youtube demo [http://www.youtube.com/watch?v=PrkrRqjUoO0 ](http://www.youtube.com/watch?v=PrkrRqjUoO0 "Youtube demo")
+
+
+License
+-------
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/Text" property="dct:title" rel="dct:type">List2csv Sublime Plugin</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/deed.en_US">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.<br />Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/avinash8526/List2CSV-Sublime-Plugin" rel="dct:source">https://github.com/avinash8526/List2CSV-Sublime-Plugin</a>.
 
 
--- list2csv.py
+++ list2csv.py
@@ -1,13 +1,54 @@
 #------------------------------------List to CSV----------------------------
 # Author: Avinash Agrawal 
 # Email : avinash8526@gmail.com
-# Version : 1.0
+# Version : 2.0
 #----------------------------------------------------------------------------
 
-import sublime, sublime_plugin
+import sublime, sublime_plugin,ast,json,os,webbrowser
 
+pdict = {}
+dir_name = None
+with open('Pattern') as patfile:
+	pdict = json.load(patfile)
+	dir_name = os.getcwd()
+
+
+def fileHandler(filename,mode=None):
+	global pdict,dir_name
+	os.chdir(dir_name)
+	if mode =='w':
+		jsonFormat =  json.dumps(pdict, sort_keys = True, indent = 4)
+		l = open(filename, mode)
+		l.write(jsonFormat)
+		l.close()
+	else:
+		with open(filename) as patfile:
+	    		if filename == "Pattern":
+	    			pdict = json.load(patfile)
+	    		return patfile	
+
+def on_loadP():
+	global fileHandler,pdict
+	if pdict["ShowPop"] == "Yes":
+		sublime.message_dialog("Plugin Notification :\n\nList2CSv Sublime PLugin is updated, View Read Me File for detailed info.\n\n\t\t\t\t\t \n\n\nThis popup will not open again.")	
+		try:
+			print os.getcwd()
+			f = open("README.md",'r')
+			print f.read()
+			webbrowser.open("https://github.com/avinash8526/List2CSV-Sublime-Plugin/blob/master/README.md")
+		except:
+			print "Not able to open file"
+			pdict["ShowPop"] = "No"
+		finally:
+			f.close()				
+		pdict["ShowPop"] = "No"
+		fileHandler("Pattern",'w')
+
+on_loadP()
 class list2csvCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
+	global fileHandler
+
+	def run(self, edit):		
 		sels = self.view.sel()		
 		if self.isRegionEmpty(sels[0]):
 			sublime.message_dialog("Please select list by pressing ctrl+A")
@@ -19,12 +60,56 @@ class list2csvCommand(sublime_plugin.TextCommand):
 	def isRegionEmpty(self,region):
 		return region.empty()
 
-	def convertListTocsv(self,selection,edt):
-		for se in selection:			
-			positions =  self.view.lines(se)
-			for pos in positions:
-				if self.view.find('\n',pos.end()):
-					nwlnreg = self.view.find('\n',pos.end())
-					self.view.replace(edt, nwlnreg, ',')
-		return "true"			 
+	def convertListTocsv(self,selection,edt):	
+		global pdict
+		ed = 0		
+		positions =  self.view.lines(selection[0])
+		for times,pos in enumerate(positions):
+			ed = times+ed
+			self.view.insert(edt, pos.begin()+ed, pdict['FirstDel'])
+			self.view.insert(edt, pos.end()+ed+1, pdict['SecondDel'])	
+			ed = times+1
+		for pos in positions:	
+			if self.view.find('\n',pos.end()):
+				nwlnreg = self.view.find('\n',pos.end())
+				self.view.replace(edt, nwlnreg, pdict['Separator'])
+		return "true"
+							 
+		
+class l_settingsCommand(sublime_plugin.WindowCommand):
+	global fileHandler
+	
+	def run(self):
+		global pdict
+		if pdict["Memory"] == "No" or pdict["Memory"] == "NO" :
+			fileHandler("Pattern")
+		patString = (str(pdict).replace("u",'')).replace(" ",'')
+		self.window.show_input_panel("Please enter a pattern:", patString, self.on_done, None, None)
+		
+
+	def on_done(self,user_input):
+		global pdict
+		lpdict = pdict
+		pdict = ast.literal_eval(user_input.replace(' ',''))
+		for key in lpdict.keys():
+			if key not in pdict.keys():
+				self.panelO("Key is not found, refreshing pattern \nPlease put backslash followed by delimitter \\'")
+				fileHandler("Pattern")
+
+	def panelO(self,msg = None):
+		panel_name = 'test'
+		ref = self.window.get_output_panel(panel_name)
+		self.window.run_command("show_panel",{"panel": "output." + panel_name})
+		ref.set_read_only(False)
+		edit = ref.begin_edit()
+		ref.insert(edit,ref.size(),"\nError AVI0003\n")
+		ref.insert(edit,ref.size(),msg)		
+		ref.end_edit(edit)
+		ref.set_read_only(True)
+		
+	@classmethod	
+	def getClassHandler(cls):
+		return l_settingsCommand(cls)
+	
+
 		
